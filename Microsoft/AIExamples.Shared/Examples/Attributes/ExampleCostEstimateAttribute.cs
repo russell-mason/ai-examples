df04f7096e15cef082758cee092cd1f50ae454a7
Russell Mason <public@russellmason.com>
2025-12-14 12:27:27 +0000

    Fix DailyOverhead attribute assignment
    - Property DailyOverhead was not assigned correctly
    - Naming of dailyOverheadInUSD was used inconsistently (dailyCostInUsd)
    - Inconsistent use of USD and Usd
    - This is only used for informational purposes, so has no direct impact on usage

--- Microsoft/AIExamples.Shared/Examples/Attributes/ExampleCostEstimateAttribute.cs
+++ Microsoft/AIExamples.Shared/Examples/Attributes/ExampleCostEstimateAttribute.cs
@@ -18,23 +18,23 @@ public sealed class ExampleCostEstimateAttribute(double estimateInUSD, double da
     /// <summary>
     /// Specifies an estimated cost for running the example. Assumes pricing is transparent.
     /// </summary>
-    /// <param name="estimateInUsd">
+    /// <param name="estimateInUSD">
     /// A very rough estimate of the raw compute cost associated with running the example. In U.S. dollars.
     /// </param>
-    /// <param name="dailyCostInUsd">
+    /// <param name="dailyOverheadInUSD">
     /// A very rough estimate of additional daily charges associated with the resource itself, i.e. on top of the raw compute cost. In U.S. dollars.
     /// </param>
-    public ExampleCostEstimateAttribute(double estimateInUsd, double dailyCostInUsd) : this(estimateInUsd, dailyCostInUsd, CostVisibility.Transparent)
+    public ExampleCostEstimateAttribute(double estimateInUSD, double dailyOverheadInUSD) : this(estimateInUSD, dailyOverheadInUSD, CostVisibility.Transparent)
     {
     }
 
     /// <summary>
     /// Specifies an estimated cost for running the example. Assumes pricing is transparent and there are no overhead charges.
     /// </summary>
-    /// <param name="estimateInUsd">
+    /// <param name="estimateInUSD">
     /// A very rough estimate of additional daily charges associated with the resource itself, i.e. on top of the raw compute cost. In U.S. dollars.
     /// </param>
-    public ExampleCostEstimateAttribute(double estimateInUsd) : this(estimateInUsd, 0)
+    public ExampleCostEstimateAttribute(double estimateInUSD) : this(estimateInUSD, 0)
     {
     }
 
@@ -46,7 +46,7 @@ public sealed class ExampleCostEstimateAttribute(double estimateInUSD, double da
     /// <summary>
     /// A very rough estimate of additional daily charges associated with the resource itself, i.e. on top of the raw compute cost. In U.S. dollars.
     /// </summary>
-    public double DailyOverhead { get; } = estimateInUSD;
+    public double DailyOverhead { get; } = dailyOverheadInUSD;
 
     /// <summary>
     /// Whether the cost is simple to determine from the pricing pace, or whether the total cost will turn out to be a surprise (and probably not in a good way).
